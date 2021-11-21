459e621baaaca11a2060077a1f1ddf5a61021214
Simmi <simmi@live.unc.edu>
2021-11-20 20:05:12 -0500

    added missing / to mpg_data import

--- main.js
+++ main.js
@@ -1,17 +1,13 @@
 // import * as mild1 from "./src/mild/mild_1.js";
 
 // console.log(mild1.sumToString(3, 5));
-
 // console.log(mild1.getIncreasingArray(3, 7));
-
 // console.log(mild1.maxAndMin([1, 2, 3, 4, 5]));
-
 // console.log(mild1.countArray([3, 6, 3, 2, 2, 3, 'some', 'hello', 'some', [1, 2]]));
 
 // import * as mild2 from "./src/mild/mild_2.js";
 
 // console.log(mild2.identifyVariable(4));
-
 // console.log(mild2.identifyArray(['some', 3, [3, 4], false]));
 
 // let objRemoveKey = {
@@ -41,40 +37,33 @@
 // console.log(objRemoveKeys);
 // console.log(mild2.removeKeys(objRemoveKeys, ['password', 'age']));
 
-import * as medium1 from "./src/medium/medium_1.js";
+// import * as medium1 from "./src/medium/medium_1.js";
 
-console.log(medium1.getSum([1, 2, 3, 4]));
+// console.log(medium1.getSum([1, 2, 3, 4]));
+// console.log(medium1.getMedian([3,2,5,6,2,7,4,2,7,5]));
+// console.log(medium1.getStatistics([3,2,4,5,5,5,2,6,7]));
 
-console.log(medium1.getMedian([3,2,5,6,2,7,4,2,7,5]));
-
-console.log(medium1.getStatistics([3,2,4,5,5,5,2,6,7]));
+// test cases:
+// console.log(medium1.getMedian([14,2,3,47,5,5,5,7,7,7,8,9]));
+// console.log(medium1.getStatistics([14, 2, 3, 47, 5, 5, 5, 7, 7, 7, 8, 9]));
 
 // import * as medium2 from "./src/medium/medium_2.js";
 
-// console.log(medium2.
+// console.log(medium2.allCarStats);
+// console.log(medium2.moreStats);
 
-// console.log(medium2.
 
-// console.log(medium2.
+import * as medium3 from "./src/medium/medium_3.js";
+import mpg_data from "./src/medium/data/mpg_data.js";
 
-// console.log(medium2.
-
-// import * as medium3 from "./src/medium/medium_3.js";
-
-// console.log(medium3.
-
-// console.log(medium3.
-
-// console.log(medium3.
-
-// console.log(medium3.
+console.log(medium3.searchHighPower(mpg_data, 200, 200));
+console.log(medium3.searchMpg(mpg_data, 16, 20));
+console.log(medium3.searchName(mpg_data, "auto"));
+console.log(medium3.searchByYear(mpg_data, [2018, 2019]));
 
 // import * as spicy from "./src/spicy/spicy_9.js";
 
 // console.log(spicy.
-
 // console.log(spicy.
-
 // console.log(spicy.
-
 // console.log(spicy.
